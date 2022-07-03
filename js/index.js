f64fcbfbb52059d599aec338d0f82abf47a81883
Tornike Epitashvili <itorn9777@gmail.com>
2022-07-03 20:23:25 +0400

    finished footer, added meta tags

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 import "https://unpkg.com/swiper/swiper-bundle.min.js";
 
 const swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-      },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
