41b6e0ff64ccba898796937b829d519c33655bdf
Tornike Epitashvili <itorn9777@gmail.com>
2022-07-03 21:16:56 +0400

    added burger menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
+// import swiperjs and initialize slider
 import "https://unpkg.com/swiper/swiper-bundle.min.js";
-
 const swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -9,3 +9,25 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// burger menu
+let open = false;
+window.onclick = (e) => {
+  const openMenu = document.getElementById('menu-btn');
+  const menuWrapper = document.getElementById('menu-wrapper');
+  let burgerOuter = e.target != menuWrapper;
+  
+  // if closed and mouse target equals burger icon, open burger menu,
+  // if opened and mouse target does not equal burger content, close burger menu.
+  if(!open) {
+    if(e.target == openMenu) {
+      menuWrapper.style.right = '0';
+      openMenu.setAttribute('src', './images/opened-menu.png');
+      open = true;
+    }
+  } else if((open && burgerOuter)) {
+    menuWrapper.style.right = '-280px';
+    openMenu.setAttribute('src', './images/menu.png');
+    open = false;
+  }
+}
